5047b839288705d159b91198129ed41e0f9d53aa
Tony Woo <tswoo@ucdavis.edu>
2019-04-24 14:18:29 -0700

    Fix dependency issue with core-plot

---
+++ RealTimeVentView/RealTimeVentView-Bridging-Header.h
@@ -0,0 +1,13 @@
+//
+//  RealTimeVentView-Bridging-Header.h
+//  RealTimeVentView
+//
+//  Created by user149673 on 4/24/19.
+//  Copyright © 2019 CCIL. All rights reserved.
+//
+
+#ifndef RealTimeVentView_Bridging_Header_h
+#define RealTimeVentView_Bridging_Header_h
+
+
+#endif /* RealTimeVentView_Bridging_Header_h */
